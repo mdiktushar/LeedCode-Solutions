28646982b72d634ceee403fe394795f5144a37c5
Md. Imrul Kayes <48817876+mdiktushar@users.noreply.github.com>
2025-04-20 13:59:52 +0600

    Update 100. Same Tree.cpp

--- 100. Same Tree.cpp
+++ 100. Same Tree.cpp
@@ -1,13 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (p == NULL && q == NULL)
-            return true;
-        if (p == NULL || q == NULL)
-            return false;
-        if ( (p->val == q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right)) {
-                return true;
+
+    void travels(TreeNode* p, vector<long long>& vect)
+    {
+        if (p == NULL) {
+            vect.push_back(100000);
+            return;
         }
-        else return false;
+
+        vect.push_back(p->val);
+        travels(p->left, vect);
+        travels(p->right, vect);
+    }
+
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        vector<long long>v1,v2;
+        
+        travels(p, v1);
+        travels(q, v2);
+
+        return v1 == v2;
     }
 };
